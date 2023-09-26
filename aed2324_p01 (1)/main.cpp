661c6cc9b7543ae5efc8296062003e5964ef36cb
LARA\lara2 <lara2003cunha@gmail.com>
2023-09-26 11:18:49 +0100

    elimine

--- aed2324_p01 (1)/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "gtest/gtest.h"
-
-int main(int argc, char* argv[]) {
-    testing::InitGoogleTest(&argc, argv);
-    std::cout << "AED 2023/2024 - Recitation 01" << std::endl << std::endl;
-    return RUN_ALL_TESTS();
-}
