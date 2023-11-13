45d4d80d3dd9fcbad3e1722f4a2b7c925341b413
LARA\lara2 <lara2003cunha@gmail.com>
2023-11-13 18:53:11 +0000

    Update

---
+++ aed2324_p07/Tests/packagingMachine.cpp
@@ -0,0 +1,61 @@
+#include "packagingMachine.h"
+
+PackagingMachine::PackagingMachine(int boxCap): boxCapacity(boxCap)
+{}
+
+unsigned PackagingMachine::numberOfBoxes() {
+	return boxes.size();
+}
+
+unsigned PackagingMachine::addBox(Box& b1) {
+	boxes.push(b1);
+	return boxes.size();
+}
+
+HeapObj PackagingMachine::getObjects() const {
+	return this->objects;
+}
+
+HeapBox PackagingMachine::getBoxes() const {
+	return this->boxes;
+}
+
+
+//=============================================================================
+// Exercise 1: Packaging
+//=============================================================================
+// Subexercise 1.1: Load Objects
+//=============================================================================
+// TODO
+unsigned PackagingMachine::loadObjects(vector<Object> &objs) {
+	return 0;
+}
+
+
+//=============================================================================
+// Subexercise 1.2: Search Box
+//=============================================================================
+// TODO
+Box PackagingMachine::searchBox(Object& obj) {
+    return Box(0);
+}
+
+
+
+//=============================================================================
+// Subexercise 1.3: Pack Objects
+//=============================================================================
+// TODO
+unsigned PackagingMachine::packObjects() {
+	return 0;
+}
+
+
+//=============================================================================
+// Subexercise 1.4: Box with More Objects
+//=============================================================================
+// TODO
+stack<Object> PackagingMachine::boxWithMoreObjects() const {
+    stack<Object> res;
+	return res;
+}
